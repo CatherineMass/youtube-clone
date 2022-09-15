331db39cd02bcc9e79c6cfacd14652c0c92d8a76
Catherine Massarelli <catherine.massarelli@gmail.com>
2022-09-15 13:46:45 +0200

    Add react router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 // import logo from './logo.svg';
 import React from 'react';
+import { Route, Routes } from 'react-router';
 import './App.css';
+import Home from './pages/Home';
+import Playback from './pages/Playback';
 
 function App() {
 	return (
-		<div className="App">
-		</div>
+		<Routes>
+			<Route path='/' element={<Home />}>
+
+			</Route>
+			<Route path="/playback" element={<Playback />} />
+		</Routes>
 	);
 }
 
