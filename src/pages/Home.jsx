9c4176213a77e81962137cf8314fb05de6d1cea8
Catherine Massarelli <catherine.massarelli@gmail.com>
2022-09-15 14:50:02 +0200

    Add all elements of the header

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
+import { AiOutlineMenu, AiOutlineSearch, AiFillAudio } from 'react-icons/ai';
+import { BiVideoPlus, BiBell } from 'react-icons/bi';
+import logo from '../logo.svg';
 
 const Home = () => {
 	return (
 		<div className='home'>
-			<header>header</header>
+			<header>
+				<div className='header menu-logo'>
+					<AiOutlineMenu />
+					<img src={logo} />
+				</div>
+				<div className='header search'>
+					<form>
+						<input/>
+						<button type='submit'><AiOutlineSearch /></button>
+					</form>
+					<AiFillAudio />
+				</div>
+				<div className='header actions'>
+					<BiVideoPlus />
+					<BiBell />
+					<div className='avatar'><p>N</p></div>
+				</div>
+			</header>
 		</div>
 	);
 };
