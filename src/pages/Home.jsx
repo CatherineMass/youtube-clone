cf745fa8d0e7e855e4d3d8af3b7d8eec63e9f6f8
Catherine Massarelli <catherine.massarelli@gmail.com>
2022-09-15 21:45:22 +0200

    Finish header. Add sidebar and its content

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,29 +1,58 @@
 import React from 'react';
 import { AiOutlineMenu, AiOutlineSearch, AiFillAudio } from 'react-icons/ai';
 import { BiVideoPlus, BiBell } from 'react-icons/bi';
+import { BsCollectionPlay } from 'react-icons/bs';
+import { MdHomeFilled, MdOutlineExplore, MdVideoLibrary } from 'react-icons/md';
 import logo from '../logo.svg';
 
 const Home = () => {
 	return (
 		<div className='home'>
-			<header className='header'>
-				<div className='left'>
-					<AiOutlineMenu className='menu-btn' size={30} />
-					<img src={logo} />
+			<div className='sidebar'>
+				<div className='menu'>
+					<AiOutlineMenu className='menu-btn' size={25} />
 				</div>
-				<div className='center'>
-					<form>
-						<input placeholder='Search'/>
-						<button type='submit'><AiOutlineSearch /></button>
-					</form>
-					<AiFillAudio />
+				<div className='sidebar-item'>
+					<MdHomeFilled size={25} />
+					<p>Home</p>
 				</div>
-				<div className='right'>
-					<BiVideoPlus />
-					<BiBell />
-					<div className='avatar'><p>N</p></div>
+				<div className='sidebar-item'>
+					<MdOutlineExplore size={25} />
+					<p>Explore</p>
 				</div>
-			</header>
+				<div className='sidebar-item'>
+					<BsCollectionPlay size={25} />
+					<p>Subscriptions</p>
+				</div>
+				<div className='sidebar-item'>
+					<MdVideoLibrary size={25} />
+					<p>Library</p>
+				</div>
+			</div>
+			<div className='main'>
+				<header className='header'>
+					<div className='left'>
+						<img src={logo} />
+					</div>
+					<div className='center'>
+						<form className='form'>
+							<input placeholder='Search'/>
+							<div className='divider'/>
+							<div className='btn'>
+								<button type='submit'><AiOutlineSearch size={25} /></button>
+							</div>
+						</form>
+						<div className='micro'>
+							<AiFillAudio size={22} />
+						</div>
+					</div>
+					<div className='right'>
+						<BiVideoPlus size={25} />
+						<BiBell size={25} />
+						<div className='avatar'><p>N</p></div>
+					</div>
+				</header>
+			</div>
 		</div>
 	);
 };
