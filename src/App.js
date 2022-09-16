d405cde3d0548f5bc4f5f8a12f5ac28afaf388a1
Catherine Massarelli <catherine.massarelli@gmail.com>
2022-09-16 10:34:50 +0200

    Add layout page and put sidebar and header on it

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-// import logo from './logo.svg';
 import React from 'react';
 import { Route, Routes } from 'react-router';
 import './App.css';
 import Home from './pages/Home';
+import Layout from './pages/Layout';
 import Playback from './pages/Playback';
 
 function App() {
 	return (
 		<Routes>
-			<Route path='/' element={<Home />}>
-
+			<Route path='/' element={<Layout />}>
+				<Route path='home' element={<Home />} />
+				<Route path='playback' element={<Playback />} />
 			</Route>
-			<Route path="/playback" element={<Playback />} />
 		</Routes>
 	);
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,62 +1,12 @@
 import React from 'react';
-import { AiOutlineMenu, AiOutlineSearch, AiFillAudio } from 'react-icons/ai';
-import { BiVideoPlus, BiBell } from 'react-icons/bi';
-import { BsCollectionPlay } from 'react-icons/bs';
-import { MdHomeFilled, MdOutlineExplore, MdVideoLibrary } from 'react-icons/md';
-import logo from '../logo.svg';
+import { Outlet } from 'react-router';
+
 
 const Home = () => {
 	return (
 		<div className='home'>
-			<div className='sidebar'>
-				<div className='menu'>
-					<AiOutlineMenu className='menu-btn' size={25} />
-				</div>
-				<div className='sidebar-item'>
-					<MdHomeFilled size={25} />
-					<p>Home</p>
-				</div>
-				<div className='sidebar-item'>
-					<MdOutlineExplore size={25} />
-					<p>Explore</p>
-				</div>
-				<div className='sidebar-item'>
-					<BsCollectionPlay size={25} />
-					<p>Subscriptions</p>
-				</div>
-				<div className='sidebar-item'>
-					<MdVideoLibrary size={25} />
-					<p>Library</p>
-				</div>
-			</div>
-			<div className='main'>
-				<header className='header'>
-					<div className='left'>
-						<img src={logo} />
-					</div>
-					<div className='center'>
-						<form className='form'>
-							<input placeholder='Search'/>
-							<div className='search-divider'/>
-							<div className='btn'>
-								<button type='submit'><AiOutlineSearch size={25} /></button>
-							</div>
-						</form>
-						<div className='micro'>
-							<AiFillAudio size={22} />
-						</div>
-					</div>
-					<div className='right'>
-						<BiVideoPlus size={25} />
-						<BiBell size={25} />
-						<div className='avatar'><p>N</p></div>
-					</div>
-				</header>
-				<div className='divider'/>
-				<div className='main-body'>
-					
-				</div>
-			</div>
+			Home!
+			<Outlet />
 		</div>
 	);
 };
